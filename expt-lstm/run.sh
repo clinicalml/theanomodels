685f0ff1183370baee8c64c6e5be21b386ecd95d
Rahul G. Krishnan <rahulk90@gmail.com>
2016-05-29 21:16:52 -0400

    updated lstm

--- expt-lstm/run.sh
+++ expt-lstm/run.sh
@@ -1,1 +1,1 @@
-THEANO_FLAGS="lib.cnmem=0.95, scan.allow_gc=False,compiledir_format=gpu1" python2.7 train_lstm.py -sfreq 50 -ep 600 
+THEANO_FLAGS="lib.cnmem=0.95, scan.allow_gc=False,compiledir_format=gpu1" python2.7 train_lstm.py -sfreq 50 -ep 200 
