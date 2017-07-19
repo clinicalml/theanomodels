4e9c86f7b5aea25a779759ae843170497d0f184c
Rahul G. Krishnan <rahulk90@gmail.com>
2017-07-19 17:55:56 -0400

    added plotting for scatter

--- utils/viz.py
+++ utils/viz.py
@@ -9,8 +9,10 @@ def scatter2D(ax, X, Y, d_col= None, uniq_col = None, size = 10, cmap = None):
     uniq_col: None or dict [keys - unique colors, values - name of color]
             if dict, expecting d_col: (X.shape[0],)
     """
-    if uniq_col is None:
+    if uniq_col is None and d_col is None:
         ax.scatter(X, Y, s = size, cmap = cmap)
+    elif uniq_col is None and d_col is not None:
+        ax.scatter(X, Y, s = size, cmap = cmap, c = d_col)
     else:
         assert d_col is not None,'Expecting specification of colors'
         assert np.all(np.sort(np.unique(d_col))==np.sort(np.unique(uniq_col.keys()))),'Expecting all keys to have assigned colors'
